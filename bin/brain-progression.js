4f005b591abd522ce0a06ed6243032346d557839
Виктор Рунтов <runtovvs@gmail.com>
2023-02-12 22:26:59 +0300

    add ascinema 4th game in readme

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,4 +1,4 @@
-import { questionAboutName } from "../src/cli.js";
-import { getProgression } from "../games/brain-progression.js";
+import { questionAboutName } from '../src/cli.js';
+import { getProgression } from '../games/brain-progression.js';
 
 getProgression(questionAboutName());
