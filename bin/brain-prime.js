442cf925ffdf5008957dec31b5717c3ab3d4edc6
Виктор Рунтов <runtovvs@gmail.com>
2023-02-12 23:26:29 +0300

    complete 5th Game - brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+import { questionAboutName } from '../src/cli.js';
+import { isPrimeNumber } from '../games/brain-prime.js';
+
+isPrimeNumber(questionAboutName());
---
+++ games/brain-prime.js
@@ -0,0 +1,35 @@
+/* eslint-disable import/prefer-default-export */
+import readlineSync from 'readline-sync';
+
+export function isPrimeNumber(namePlayer) {
+  for (let i = 0; i < 3; i += 1) {
+    console.log(
+      'Answer "yes" if given number is prime. Otherwise answer "no".',
+    );
+
+    const randomNumber = Math.round(Math.random() * 100);
+    let divider = Math.ceil(randomNumber / 2);
+    let correctAnswer = 'yes';
+    for (divider; divider > 1; divider -= 1) {
+      if (randomNumber % divider === 0) {
+        correctAnswer = 'no';
+        break;
+      }
+    }
+    console.log(`Question: ${randomNumber}`);
+
+    const playerAnswer = readlineSync.question('Your answer: ');
+
+    if (playerAnswer === correctAnswer) {
+      console.log('Correct');
+    } else {
+      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.
+    Let's try again, ${namePlayer}!`);
+      break;
+    }
+
+    if (i === 2) {
+      console.log(`Congratulations, ${namePlayer}!`);
+    }
+  }
+}
